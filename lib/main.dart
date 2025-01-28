576e1550785dbd071c4761d07481acbe95777395
suminkim8203 <165616688+suminkim8203@users.noreply.github.com>
2025-01-28 18:03:23 +0900

    2) splash screen 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // 플러터 앱을 실행한다.
   runApp(
-    // MaterialApp 은 항상 최상위에 위치한다.
-    // Scaffold 는 바로 아래에 위치한다.
     MaterialApp(
-      // 우측 상단 디버그 배너 없애기
-      // debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            'Sumin Factory',
-            style: TextStyle(
-              color: Colors.white,
-            ),
+      home: HomeScreen(),
+    ),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF335CB0),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 32.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'asset/img/logo.png',
+              ),
+              SizedBox(
+                height: 28.0,
+              ),
+              // CircularProgressIndicator(
+              //   color: Colors.white,
+              // ),
+            ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
