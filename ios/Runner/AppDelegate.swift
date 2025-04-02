b26867a504ee52df3d1462cb8d6e29551ae83865
suminkim8203 <165616688+suminkim8203@users.noreply.github.com>
2025-04-02 23:38:19 +0900

    출퇴근 확인 앱 - GCP에서 API 키 받아오기 - Google Maps, Geolocator 플러그인 세팅 - Google Maps 화면에 띄우고 설정 적용하기

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,14 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAG1VddFhg3tYHRtOektqqD1SJ7zK_r7cc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final CameraPosition initialPosition = CameraPosition(
+    target: LatLng(
+      37.5214,
+      126.9246,
+    ),
+    zoom: 15,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: initialPosition,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
